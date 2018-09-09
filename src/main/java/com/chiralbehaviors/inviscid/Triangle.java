05b17703cdeb4e125a92206977a68656dae00408
Hellblazer <hellblazer@me.com>
2018-09-08 19:06:31 -0700

    first stuff

---
+++ src/main/java/com/chiralbehaviors/inviscid/Triangle.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2018 Chiral Behaviors, LLC. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.chiralbehaviors.inviscid;
+
+import javafx.geometry.Point3D;
+
+/**
+ * @author halhildebrand
+ *
+ */
+public class Triangle {
+    public final Point3D a;
+    public final Point3D b;
+    public final Point3D c;
+
+    public Triangle(Point3D a, Point3D b, Point3D c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
---
+++ src/test/java/com/chiralbehaviors/inviscid/TestHarness.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2018 Chiral Behaviors, LLC. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.chiralbehaviors.inviscid;
+
+import com.chiralbehaviors.jfx.viewer3d.ContentModel;
+import com.chiralbehaviors.jfx.viewer3d.Jfx3dViewerApp;
+
+/**
+ * @author halhildebrand
+ *
+ */
+public class TestHarness extends Jfx3dViewerApp {
+
+    @Override
+    protected ContentModel createContentModel() {
+        return super.createContentModel();
+    }
+}
