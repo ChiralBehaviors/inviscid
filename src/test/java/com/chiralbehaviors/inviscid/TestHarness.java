5326d519ce6556c2d92de11c2b1e30b2c60f0f42
Hellblazer <hellblazer@me.com>
2018-09-16 17:13:11 -0700

    rotate and translate a face

--- src/test/java/com/chiralbehaviors/inviscid/TestHarness.java
+++ src/test/java/com/chiralbehaviors/inviscid/TestHarness.java
@@ -31,14 +31,12 @@ import static javafx.scene.paint.Color.RED;
 import static javafx.scene.paint.Color.VIOLET;
 import static javafx.scene.paint.Color.YELLOW;
 
-import java.util.List;
-
-import javax.swing.plaf.metal.OceanTheme;
 import javax.vecmath.Vector3d;
 
 import com.javafx.experiments.jfx3dviewer.ContentModel;
 import com.javafx.experiments.jfx3dviewer.Jfx3dViewerApp;
 
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.paint.Material;
 import javafx.scene.paint.PhongMaterial;
@@ -46,11 +44,10 @@ import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import mesh.Face;
 import mesh.polyhedra.Polyhedron;
-import mesh.polyhedra.plato.Cube;
 import mesh.polyhedra.plato.Octahedron;
-import mesh.polyhedra.plato.PlatonicSolid;
 import mesh.polyhedra.plato.Tetrahedron;
 
 /**
@@ -169,30 +166,44 @@ public class TestHarness extends Jfx3dViewerApp {
 
         ContentModel content = getContentModel();
         Group group = new Group();
-        Cube[] cube = PhiCoordinates.cubes();
-        Tetrahedron[] tet = PhiCoordinates.tetrahedrons();
-        Octahedron[] oct = PhiCoordinates.octahedrons();
 
-        //        addPolyhedron(group, tet[4], redMaterial, materials);
-        //        addPolyhedron(group, tet[5], blueMaterial, materials);
+        Octahedron oct = PhiCoordinates.octahedrons()[0];
+        Face f = oct.getFaces()
+                       .get(0);
+        Vector3d centroid = f.centroid();
 
-        //        int i = 0;
-        //        for (Face face : oct[2].getFaces()) {
-        //            MeshView mesh = face.constructMeshView();
-        //            mesh.setMaterial(eight4Materials[i++]);
-        //            group.getChildren()
-        //                 .add(mesh);
-        //        }
-
-//        addPolyhedron(group, oct[0], greenMaterial, materials);
-//        addPolyhedron(group, oct[1], yellowMaterial, materials);
-      addPolyhedron(group, tet[0], redMaterial, eight4Materials);
-      addPolyhedron(group, tet[1], blueMaterial, eight4Materials);
-      addPolyhedron(group, oct[0], greenMaterial, eight4Materials);
-      addPolyhedron(group, oct[1], yellowMaterial, eight4Materials);
-//        addFaces(group, oct[0], eight4Materials);
-//        addFaces(group, oct[1], eight4Materials);
- 
+        Tetrahedron tet1 = PhiCoordinates.tetrahedrons()[0];
+        
+        Vector3d axis = tet1.getVertexPositions().get(0);
+        
+        Rotate rotation = new Rotate();
+        rotation.setAngle(30);
+        
+        rotation.setPivotX(centroid.x);
+        rotation.setPivotY(centroid.y);
+        rotation.setPivotZ(centroid.z);
+        
+        rotation.setAxis(new Point3D(axis.x, axis.y, axis.z));
+        
+        Vector3d direction = new Vector3d(); 
+        direction.normalize(axis);
+        direction.scale(1.5);
+        
+        Translate translate = new Translate();
+        translate.setX(direction.x);
+        translate.setY(direction.y);
+        translate.setZ(direction.z);
+       
+        
+        MeshView face = f.constructMeshView();
+        face.setMaterial(redMaterial);
+        group.getChildren().add(face);
+        
+        MeshView rotated = f.constructMeshView(); 
+        rotated.setMaterial(blueMaterial);
+        rotated.getTransforms().addAll(rotation, translate);
+        
+        group.getChildren().add(rotated);
         content.setContent(group);
     }
 }
